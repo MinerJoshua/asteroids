61d8c8e86dc477a8175d78106a259ce18c9a815f
Joshua Miner Laptop <joshuaminer@laptop.com>
2024-09-25 08:13:30 +0100

    bubu y

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *;
+from player import Player;
 import pygame;
 
 def main():
@@ -6,12 +7,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT));
     clock = pygame.time.Clock();
     delta_time:int = 0;
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black");
+        player.update(delta_time);
+        player.draw(screen);
         pygame.display.flip();
         delta_time = clock.tick(60)/1000;
 
